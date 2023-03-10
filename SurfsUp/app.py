e032b7c06a9f13b82689531bade78807155cc81b
equitymarkets <derekprice@Dereks-MacBook-Air.local>
2023-03-09 19:20:29 -0500

    rough draft, refactor first plt grasph

---
+++ SurfsUp/app.py
@@ -0,0 +1,116 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+from datetime import datetime, timedelta 
+
+from flask import Flask, jsonify
+
+#CHANGE THIS!!!!!!
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
+
+Base = automap_base()
+
+Base.prepare(autoload_with=engine)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Hawaii <<<<< climate app.</br>"
+        f"</br>"
+        f"<p>Directions:</p></br>"
+        f"Paste the desired string onto the end of your current URL for selected precipitation, weather station, and temperature data.</br>"
+        f"/api/v1.0/precipitation</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/tobs</br>"
+        f"</br>"
+        f"To query minimum, average, and maximum values, append the start date in form XXXX-XX-XX to the below URL and append it to the end of your current URL. If you would like to specify and end date, add an end date to the end in the form /XXXX-XX-XX"
+        f"</br>"
+        f"/api/v1.0/"
+
+
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precip():
+    session = Session(engine)
+    
+    year_precipitation = session.query(Measurement.date, Measurement.prcp)\
+        .filter(Measurement.date >= datetime.strptime(session.query(func.max(Measurement.date))\
+        .scalar(), '%Y-%m-%d').date() - dt.timedelta(days=365))\
+        .order_by(Measurement.date.asc()).all()
+    
+    session.close()
+
+    data_plot = list(np.ravel(year_precipitation))
+
+    return jsonify(data_plot)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    most_active_stations = sorted(session.query(Measurement.station, func.count(Measurement.station)).\
+                         group_by(Measurement.station).all(), key = lambda x: x[1], reverse=True)
+    
+    session.close()
+
+    station_plot = list(np.ravel(most_active_stations))
+
+    return jsonify(station_plot)
+
+@app.route("/api/v1.0/tobs")
+def temps():
+    session = Session(engine)
+
+    most_active_station_id = sorted(session.query(Measurement.station, func.count(Measurement.station)).\
+                         group_by(Measurement.station).all(), key = lambda x: x[1], reverse=True)[0][0]
+    
+    year_temperature = session.query(Measurement.date, Measurement.tobs)\
+        .filter(Measurement.date >= datetime.strptime(session.query(func.max(Measurement.date))\
+        .scalar(), '%Y-%m-%d').date() - dt.timedelta(days=365))\
+        .filter(Measurement.station == most_active_station_id).all()
+    
+    session.close()
+
+    temp_plot = list(np.ravel(year_temperature))
+    
+    return jsonify(temp_plot)
+
+@app.route("/api/v1.0/<start>")
+def query_from_start(start):
+    session = Session(engine)
+    
+    statistics_start = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+           .filter(Measurement.date >= start ).all()
+
+    output = { "min_temp" : statistics_start[0][0], "avg_temp" : round(statistics_start[0][1], 1), "max_temp" : statistics_start[0][2]}
+
+    session.close()
+
+    #return(f"{output}")
+    return jsonify(output)
+@app.route("/api/v1.0/<start>/<end>")
+def query_start_to_end(start, end):
+    session = Session(engine)
+
+    statistics_start_end = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+            .filter(Measurement.date >= start)\
+            .filter(Measurement.date <= end).all()
+    output = { "min_temp" : statistics_start_end[0][0], "avg_temp" : round(statistics_start_end[0][1], 1), "max_temp" : statistics_start_end[0][2]}
+
+    
+    session.close()
+
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
